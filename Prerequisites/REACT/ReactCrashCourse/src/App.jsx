bc4d3995f3301bc59d17637252e05a0aa760ccab
Faiazur Rahman <faiazurrahman748@gmail.com>
2025-06-28 16:28:07 +0600

    test

--- Prerequisites/REACT/ReactCrashCourse/src/App.jsx
+++ Prerequisites/REACT/ReactCrashCourse/src/App.jsx
@@ -6,9 +6,11 @@ import Jobspage from './Pages/Jobspage'
 import Notfoundpage from './Pages/Notfoundpage'
 import Jobpage, {jobLoader} from './Pages/Jobpage'
 import Addjobpage from './Pages/Addjobpage'
+import EditJobPage from './Pages/EditJobPage'
 
 
 const App = ()=>{
+  //addnewjob
 const addjob= async (newJob)=>{
   const response = await fetch('/api/jobs',{
     method: "post",
@@ -21,18 +23,44 @@ const addjob= async (newJob)=>{
   return;
 };
 
+//deletejob
+
+const deleteJob = async (id)=>{
+  const response = await fetch(`/api/jobs/${id}`,{
+    method: "DELETE",
+  
+  });
+  return;
+
+};
+
+//update job
+
+const updateJob = async (job)=>{
+  const response = await fetch(`/api/jobs/$(job.id)`,{
+    method: "PUT",
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(job),
+  
+  });
+  return;
+}
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Mainlayout/>}>
           <Route index element={<Homepage />} />
-          <Route path='/jobs' element={<Jobspage/>}/>
+          <Route path='/jobs' element={<Jobspage />}/>
           {/* <Route path='/jobs/:id' element={<Jobpage/>}/> */}
-          <Route path='/jobs/:id' element={<Jobpage/>} loader={jobLoader}/>
+          <Route path='/jobs/:id' element={<Jobpage deleteJob={deleteJob}/>} loader={jobLoader}/>
 
           <Route path='/add-job' element={<Addjobpage addJobSubmit={addjob}/>}/>
           {/* whenevr I fire this function, that function above will fire off too */}
+
+          <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader}/>
           
           <Route path='*' element={<Notfoundpage/>}/>
           
--- Prerequisites/REACT/ReactCrashCourse/src/Pages/EditJobPage.jsx
+++ Prerequisites/REACT/ReactCrashCourse/src/Pages/EditJobPage.jsx
@@ -1,45 +1,45 @@
 import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';   //to redirect to the jobs page this hook is used
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom'
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
-
-const Addjobpage = ({addJobSubmit}) => {     //props to pass in route
-    const [title, setTitle] = useState('');
-    const [type, setType] = useState('Full-Time');
-    const [location,setLocation] = useState('');
-    const [description,setDescription] = useState('');
-    const [salary,setSalary] = useState('Under $50K');
-    const [companyName,setCompanyName] = useState('');
-    const [companyDescription,setCompanyDescription] = useState('');
-    const [contactEmail, setContactEmail] = useState('');
-    const [contactPhone, setContactPhone] = useState('');
-
-
+const EditJobPage = ({updateJobSubmit}) => {
+    const job = useLoaderData();
     const navigate = useNavigate();
-    const submitForm=(e)=>{
-        e.preventDefault();    //to stop an event from hapenning
+    const {id} = useParams();
 
-        const newjob={
-            title,
-            type,
-            location,
-            description,
-            salary,
-            company:{
-                companyName,
-                companyDescription,
-                contactEmail,
-                contactPhone,
-            },
-        };
-        addJobSubmit(newjob);
-        toast.success('Job Added Successfully')
-        return navigate('/jobs');
-    }
-//all these to redirect to jobs page after submitting form
+     const [title, setTitle] = useState(job.title);
+        const [type, setType] = useState(job.type);
+        const [location,setLocation] = useState(job.location);
+        const [description,setDescription] = useState(job.description);
+        const [salary,setSalary] = useState(job.salary);
+        const [companyName,setCompanyName] = useState(job.company.name);
+        const [companyDescription,setCompanyDescription] = useState(job.company.description);
+        const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+        const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
+        const submitForm=(e)=>{
+                e.preventDefault();    //to stop an event from hapenning
+        
+                const updatedjob={
+                    title,
+                    type,
+                    location,
+                    description,
+                    salary,
+                    company: {
+                          name: companyName,
+                          description: companyDescription,
+                          contactEmail,
+                          contactPhone,
+}
 
+                    },
+                };
+                updateJobSubmit(updatedjob);
+                toast.success('Job updated Successfully')
+                return navigate(`/jobs/${id}`);
+            }
   return (
     <>
     <section className="bg-indigo-50">
@@ -48,7 +48,7 @@ const Addjobpage = ({addJobSubmit}) => {     //props to pass in route
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
           <form onSubmit={submitForm}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
@@ -216,7 +216,7 @@ const Addjobpage = ({addJobSubmit}) => {     //props to pass in route
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
@@ -227,4 +227,4 @@ const Addjobpage = ({addJobSubmit}) => {     //props to pass in route
   )
 }
 
-export default Addjobpage
+export default EditJobPage
